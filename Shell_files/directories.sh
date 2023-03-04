04f4d535ee7d3c26f58a8e8c86c0c7ff89427a91
JonMS95 <jon_portugalete@hotmail.com>
2023-03-04 15:57:43 +0100

    Upgraded directories.sh file. It now created every directory based in the srtucture shown within config.xml file.

--- Shell_files/directories.sh
+++ Shell_files/directories.sh
@@ -1,5 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ ! -d Dynamic_libraries ]; then
-    mkdir Dynamic_libraries
+DIR_KEYWORD="Directories/"
+PATH_CONFIG="config.xml"
+PATH_DIR_LIST="Temp/directory_list.txt"
+DIR_PREFIX="config/Directories/"
+
+if [ ! -d Temp ]; then
+    mkdir Temp
+fi
+
+xmlstarlet el $PATH_CONFIG | grep $DIR_KEYWORD >> $PATH_DIR_LIST
+
+while read -r line
+do
+    new_dir=${line/#$DIR_PREFIX}
+    if [ ! -d $new_dir ]
+    then
+        echo "Creating $new_dir directory ..."
+        mkdir $new_dir
+    else
+        echo "$new_dir directory already exists"
+    fi
+done < $PATH_DIR_LIST
+
+if [ -d Temp ]; then
+    rm -rf Temp
 fi
