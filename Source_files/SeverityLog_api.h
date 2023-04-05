3cb021195c4255930968cc8d3b71b819889df249
JonMS95 <jon_portugalete@hotmail.com>
2023-04-05 12:15:25 +0200

    Added severity level masks.

--- Source_files/SeverityLog_api.h
+++ Source_files/SeverityLog_api.h
@@ -4,7 +4,14 @@
 #define SVRTY_LVL_ERR   1
 #define SVRTY_LVL_INF   2
 #define SVRTY_LVL_WNG   3
-#define SVRTY_LVL_DBG   4   
+#define SVRTY_LVL_DBG   4
+
+#define SVRTY_LOG_MASK_OFF  0b0000
+#define SVRTY_LOG_MASK_ERR  0b0001
+#define SVRTY_LOG_MASK_INF  0b0010
+#define SVRTY_LOG_MASK_WNG  0b0100
+#define SVRTY_LOG_MASK_DBG  0b1000
+#define SVRTY_LOG_MASK_ALL  0b1111
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
 /// @brief Prints a log with different color and initial string depending on the severity level.
