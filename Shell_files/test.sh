5db1a1bd4bcba9415dbaad970840ecfbab582ab0
JonMS95 <jon_portugalete@hotmail.com>
2023-10-08 23:41:13 +0200

    Added test shell file.

---
+++ Shell_files/test.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+export LD_LIBRARY_PATH=~/Desktop/scripts/C/C_Severity_Log/Tests/Dependency_files/Dynamic_libraries
+
+echo $(pwd)
+
+echo
+echo "*******************************"
+echo "Testing 'main' executable file."
+echo "*******************************"
+
+./Tests/Executable_files/main
