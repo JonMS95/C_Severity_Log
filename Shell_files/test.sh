2b7e097716210a3b08d98d3a9cabdfc25e464468
JonMS95 <jon_portugalete@hotmail.com>
2024-07-31 17:08:26 +0200

    Modified test file so that no problem happens regarding tilde symbol expansion.

--- Shell_files/test.sh
+++ Shell_files/test.sh
@@ -1,8 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-export LD_LIBRARY_PATH=~/Desktop/scripts/C/C_Severity_Log/Tests/Dependency_files/Dynamic_libraries
+CONFIG_FILE="config.xml"
 
-echo $(pwd)
+PATH_TO_THIS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PATH_TO_LIB_ROOT="$(dirname ${PATH_TO_THIS})"
+PATH_TO_TEST_DEPS="$( xmlstarlet sel -t -v "config/Tests/Dependencies/@Dest" ${CONFIG_FILE})"
+PATH_TO_TEST_DEP_DYN_LIBS=${PATH_TO_LIB_ROOT}/${PATH_TO_TEST_DEPS}/Dynamic_libraries
+
+export LD_LIBRARY_PATH=${PATH_TO_TEST_DEP_DYN_LIBS}
 
 echo
 echo "*******************************"
