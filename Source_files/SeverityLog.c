74d70c30ff542d70c7fed57ef1c2984e3749f7c1
JonMS95 <jon_portugalete@hotmail.com>
2025-01-18 17:57:14 +0100

    Added several define statements as well as SetSeverityLogPrintExeNameStatus and PrintCallingExeFileName functions.

--- Source_files/SeverityLog.c
+++ Source_files/SeverityLog.c
@@ -3,12 +3,49 @@
 /************************************/
 
 #include <stdio.h>
+#include <string.h>
 #include <stdarg.h>
 #include <time.h>
 #include <stdbool.h>
-#include "SeverityLog.h"
+#include <execinfo.h>
+#include <stdlib.h>
+#include <libgen.h>
+#include "SeverityLog_api.h"
 
 /************************************/
+
+/***********************************/
+/******** Define statements ********/
+/***********************************/
+
+#define SVRTY_CRLF      "\r\n"
+#define SVRTY_STR_END   '\0'
+
+#define SVRTY_CLR_BASE  30 // Black color.
+
+#define SVRTY_CHG_CLR       "\033[0;%dm"
+#define SVRTY_RST_CLR       "\033[0m"
+
+#define SVRTY_STR_ERR       "[ERR] "
+#define SVRTY_STR_INF       "[INF] "
+#define SVRTY_STR_WNG       "[WNG] "
+#define SVRTY_STR_DBG       "[DBG] "
+
+#define SVRTY_TIME_DATE_SIZE    128
+#define SVRTY_TIME_DATE_FORMAT  "[%c] "
+
+#define SVRTY_LOG_SUCCESS           0
+#define SVRTY_LOG_WNG_SILENT_LVL    -1
+
+#define SVRTY_EXE_FILE_STACK_SIZE       3
+#define SVRTY_EXE_FILE_STACK_LVL        2
+#define SVRTY_EXE_FILE_ADDR_PREFIX      '('
+#define SVRTY_EXE_FILE_SO_SUFFIX        ".so"
+#define SVRTY_EXE_FILE_SO_PREFIX        "lib"
+#define SVRTY_EXE_FILE_SO_PREFIX_IDX    3
+#define SVRTY_EXE_FILE_FORMAT           "[%s] "
+
+/***********************************/
 
 /***********************************/
 /******** Private variables ********/
@@ -16,8 +53,22 @@
 
 static int  severity_log_mask   = SVRTY_LOG_MASK_EIW;
 static bool print_time_status   = false;
+static bool print_exe_file_name = false;
 
 /***********************************/
+
+/*************************************/
+/**** Private function prototypes ****/
+/*************************************/
+
+static void ChangeSeverityColor(int severity);
+static void ResetSeverityColor(void);
+static void PrintSeverityLevel(int severity);
+static void PrintTime(void);
+static void PrintCallingExeFileName(void);
+static int CheckSeverityLogMask(int severity);
+
+/*************************************/
 
 /*************************************/
 /******* Function definitions ********/
@@ -116,7 +167,7 @@ void SetSeverityLogPrintTimeStatus(bool time_status)
 ////////////////////////////////////////////////////////////////////////////////////////// 
 /// @brief If print_time_status == true, it prints time (includes date) in local timezone.
 //////////////////////////////////////////////////////////////////////////////////////////
-void PrintTime(void)
+static void PrintTime(void)
 {
     if(!print_time_status)
         return;
@@ -130,9 +181,61 @@ void PrintTime(void)
     if (time_info == NULL)
         return;
 
-    char time_str[128];
-    strftime(time_str, sizeof(time_str), SVRTY_STR_TIME_DATE, time_info);
+    char time_str[SVRTY_TIME_DATE_SIZE];
+    strftime(time_str, sizeof(time_str), SVRTY_TIME_DATE_FORMAT, time_info);
     printf("%s", time_str);
+}
+
+/////////////////////////////////////////////////////////////
+/// @brief Set value of print_exe_file_name private variable.
+/// @param exe_name_status Target status value (T/F).
+/////////////////////////////////////////////////////////////
+void SetSeverityLogPrintExeNameStatus(bool exe_name_status)
+{
+    print_exe_file_name = exe_name_status;
+}
+
+///////////////////////////////////////////////////////////////////////////////////// 
+/// @brief If print_exe_file_name == true, it print the calling executable file name.
+/////////////////////////////////////////////////////////////////////////////////////
+static void PrintCallingExeFileName(void)
+{
+    if(!print_exe_file_name)
+        return;
+
+    void *buffer[SVRTY_EXE_FILE_STACK_SIZE];
+    int size = backtrace(buffer, SVRTY_EXE_FILE_STACK_SIZE);  // Capture the call stack
+    char **symbols = backtrace_symbols(buffer, size);  // Resolve the symbols
+    
+    if (size >= SVRTY_EXE_FILE_STACK_LVL)
+    {
+        char *symbol = symbols[SVRTY_EXE_FILE_STACK_LVL];  // The calling function (1st index in backtrace)
+        
+        // Look for the first '(' character, which indicates the start of the memory address
+        char *start_of_function_name = symbol;
+        char *end_of_function_name = strchr(start_of_function_name, SVRTY_EXE_FILE_ADDR_PREFIX);  // Find the '('
+        
+        if (end_of_function_name) {
+            *end_of_function_name = SVRTY_STR_END;
+        }
+        
+        char* file_name = basename(start_of_function_name);
+
+        char *so_extension = strstr(file_name, SVRTY_EXE_FILE_SO_SUFFIX);
+        if (so_extension) {
+            *so_extension = SVRTY_STR_END;
+
+            if (strncmp(file_name, SVRTY_EXE_FILE_SO_PREFIX, SVRTY_EXE_FILE_SO_PREFIX_IDX) == 0) {
+                file_name += SVRTY_EXE_FILE_SO_PREFIX_IDX;
+            }
+        }
+
+        // Print just the function name
+        printf(SVRTY_EXE_FILE_FORMAT, file_name);  
+    }
+
+    if (symbols != NULL)
+        free(symbols);
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
@@ -157,8 +260,8 @@ int SeverityLog(int severity, const char* format, ...)
     ChangeSeverityColor(severity);
 
     PrintTime();
-
     PrintSeverityLevel(severity);
+    PrintCallingExeFileName();
 
     va_start(args, format);
     done = vfprintf(stdout, format, args);
@@ -166,7 +269,7 @@ int SeverityLog(int severity, const char* format, ...)
 
     ResetSeverityColor();
 
-    printf("\r\n");
+    printf(SVRTY_CRLF);
 
     return done;
 }
